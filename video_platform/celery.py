f522d8c81fae4964473ad0c65955baadd337e82d
ItsOrv <poriya.saw@gmail.com>
2025-12-14 13:36:28 +0330

    Replace print statement with logging in Celery debug task

--- video_platform/celery.py
+++ video_platform/celery.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 import os
+import logging
 from celery import Celery
 
 # تنظیمات پروژه
@@ -13,6 +14,8 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # شناسایی خودکار وظایف (tasks)
 app.autodiscover_tasks()
 
+logger = logging.getLogger(__name__)
+
 @app.task(bind=True)
 def debug_task(self):
-    print(f'Request: {self.request!r}')
+    logger.debug(f'Request: {self.request!r}')
