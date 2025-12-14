0a7df7be409cf2b28148ba622993de98c7549502
ItsOrv <poriya.saw@gmail.com>
2025-12-14 13:36:30 +0330

    Enhance VideoSerializer with nested data and computed fields

--- videos/serializers.py
+++ videos/serializers.py
@@ -1,7 +1,40 @@
 from rest_framework import serializers
 from .models import Video
 
+
 class VideoSerializer(serializers.ModelSerializer):
+    """Serializer for Video model"""
+    uploaded_by_username = serializers.CharField(source='uploaded_by.username', read_only=True)
+    category_name = serializers.CharField(source='category.name', read_only=True)
+    tags_list = serializers.SerializerMethodField()
+    likes_count = serializers.SerializerMethodField()
+    dislikes_count = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
+    
     class Meta:
         model = Video
-        fields = '__all__'
+        fields = [
+            'id', 'title', 'description', 'file', 'thumbnail',
+            'uploaded_by', 'uploaded_by_username', 'uploaded_at',
+            'is_premium', 'price', 'is_active', 'is_featured',
+            'views_count', 'duration', 'category', 'category_name',
+            'tags', 'tags_list', 'likes_count', 'dislikes_count',
+            'comments_count'
+        ]
+        read_only_fields = ['uploaded_by', 'uploaded_at', 'views_count']
+    
+    def get_tags_list(self, obj):
+        """Return list of tag names"""
+        return [tag.name for tag in obj.tags.all()]
+    
+    def get_likes_count(self, obj):
+        """Return likes count"""
+        return obj.get_likes_count()
+    
+    def get_dislikes_count(self, obj):
+        """Return dislikes count"""
+        return obj.get_dislikes_count()
+    
+    def get_comments_count(self, obj):
+        """Return comments count"""
+        return obj.get_comments_count()
