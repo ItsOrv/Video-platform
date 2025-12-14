8677f8758fa8bb1f12ab5594f4e5e3b16b6a92c1
ItsOrv <poriya.saw@gmail.com>
2025-12-14 13:37:16 +0330

    Add save_user_profile signal handler

--- accounts/signals.py
+++ accounts/signals.py
@@ -9,3 +9,10 @@ def create_user_profile(sender, instance, created, **kwargs):
     """Create UserProfile when a new user is created"""
     if created:
         UserProfile.objects.get_or_create(user=instance)
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def save_user_profile(sender, instance, **kwargs):
+    """Save UserProfile when user is saved"""
+    if hasattr(instance, 'profile'):
+        instance.profile.save()
